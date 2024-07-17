cb3eea1c21dc54712e7fb648d0bb720abcb2837a
anand-KV <149510501+anand-KV@users.noreply.github.com>
2024-07-17 20:01:54 +0530

    Add files via upload

--- JAVA/cpumain.java
+++ JAVA/cpumain.java
@@ -1,54 +1,71 @@
+/*create a class cpu with attribute price.create an inner class processor
+(no of cores,manufacturer)and a static nested class ram(memory,manufacturer)
+create an object of the cpu and print info of processor and ram.*/
+
+import java.util.*;
 class cpu
 {
-	float price;
-	cpu(float p)
-	{
-		price=p;
-	}
-	class processor
-	{
-		int no_of_cores;
-		String manufacturer;
-		processor(int c,String m)
-		{
-			no_of_cores=c;
-			manufacturer=m;
-		}
-		void pdisplay()
-		{
-			System.out.println("no of cores:"+no_of_cores);
-			System.out.println("processor manufacturer:"+manufacturer);
-		}
-	}
-	static class ram
-	{
-		String memory;
-		String manufacturer;
-		ram(String m,String n)
-		{
-			memory=m;
-			manufacturer=n;
-		}
-		void rdisplay()
-		{
-			System.out.println("memory:"+memory);
-			System.out.println("ram manufacturer:"+manufacturer);
-		}
-	}
-	void display()
-	{
-		System.out.println("price:"+price);
-	}
+    Double price;
+    cpu(Double p)
+    {
+        price=p;
+    }
+    class processor
+    {
+        int noc;
+        String manu;
+        processor(int c,String m)
+        {
+          noc=c;
+          manu=m;
+        }
+        void pdisplay()
+        {
+            System.out.println("no of cores:"+noc);
+            System.out.println("manufacturer:"+manu);
+        }
+    }
+
+ static class ram
+ {
+    String memo;
+    String manuf;
+    ram(String me,String ma)
+    {
+        memo=me;
+        manuf=ma;
+    }
+    void rdisplay()
+    {
+        System.out.println("memory:"+memo);
+        System.out.println(" ram manufacturer:"+manuf);
+    }
+ }
+    void display()
+    {
+      System.out.println("price:"+price);
+    }
 }
 class cpumain
 {
-	public static void main(String arg[])
-	{
-		cpu c=new cpu(100);
-		c.display();
-		cpu.processor p=c.new processor(3,"kv");
-		p.pdisplay();
-		cpu.ram r=new cpu.ram("7gb","anand");
-		r.rdisplay();
-	}
+    public static void main(String ar[])
+    {
+        Scanner s=new Scanner(System.in);
+        Scanner s1=new Scanner(System.in);
+        System.out.println("enter the price:");
+        double pr=s.nextDouble();
+        cpu c=new cpu(pr);
+        System.out.println("enter the noc and manu:");
+        int nc=s.nextInt();
+        String mn=s1.nextLine();
+        cpu.processor p= c.new processor(nc,mn);
+        System.out.println("enter the memory and manf:");
+        String mm=s1.nextLine();
+        String mnf=s1.nextLine();
+        cpu.ram r=new cpu.ram(mm,mnf);
+        r.rdisplay();
+        p.pdisplay();
+        c.display();
+
+    }
 }
